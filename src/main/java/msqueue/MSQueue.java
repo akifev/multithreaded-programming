69a91abee94ce092b7e5649af2d45fa948349911
Akifev Daniil <akifevdaniil@yandex.ru>
2019-10-08 23:06:32 +0300

    first commit

--- src/main/java/msqueue/MSQueue.java
+++ src/main/java/msqueue/MSQueue.java
@@ -1,39 +1,78 @@
 package msqueue;
 
+import kotlinx.atomicfu.AtomicRef;
+
 public class MSQueue implements Queue {
     private Node head;
     private Node tail;
+
+    private enum Status {CHANGING, VALID}
+
+    private AtomicRef<Status> headStatus;
+    private AtomicRef<Status> tailStatus;
 
     public MSQueue() {
         Node dummy = new Node(0);
         this.head = dummy;
         this.tail = dummy;
+        this.headStatus = new AtomicRef<>(Status.VALID);
+        this.tailStatus = new AtomicRef<>(Status.VALID);
     }
 
     @Override
     public void enqueue(int x) {
-        Node newTail = new Node(x);
-        tail.next = newTail;
-        tail = newTail;
+        while (true) {
+            if (tailStatus.compareAndSet(Status.VALID, Status.CHANGING)) {
+                Node newTail = new Node(x);
+                tail.next = newTail;
+                tail = newTail;
+                tailStatus.compareAndSet(Status.CHANGING, Status.VALID);
+                return;
+            }
+        }
     }
 
     @Override
     public int dequeue() {
-        Node curHead = head;
-        Node next = head.next;
-        if (curHead == tail)
-            return Integer.MIN_VALUE;
-        head = next;
-        return next.x;
+        while (true) {
+            if (headStatus.compareAndSet(Status.VALID, Status.CHANGING)) {
+                while (true) {
+                    if (tailStatus.compareAndSet(Status.VALID, Status.CHANGING)) {
+                        int result = Integer.MIN_VALUE;
+                        Node curHead = head;
+                        if (curHead != tail) {
+                            Node next = head.next;
+                            head = next;
+                            result = next.x;
+                        }
+                        tailStatus.compareAndSet(Status.CHANGING, Status.VALID);
+                        headStatus.compareAndSet(Status.CHANGING, Status.VALID);
+                        return result;
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public int peek() {
-        Node curHead = head;
-        Node next = head.next;
-        if (curHead == tail)
-            return Integer.MIN_VALUE;
-        return next.x;
+        while (true) {
+            if (headStatus.compareAndSet(Status.VALID, Status.CHANGING)) {
+                while (true) {
+                    if (tailStatus.compareAndSet(Status.VALID, Status.CHANGING)) {
+                        Node curHead = head;
+                        int result = Integer.MIN_VALUE;
+                        if (curHead != tail) {
+                            Node next = head.next;
+                            result = next.x;
+                        }
+                        tailStatus.compareAndSet(Status.CHANGING, Status.VALID);
+                        headStatus.compareAndSet(Status.CHANGING, Status.VALID);
+                        return result;
+                    }
+                }
+            }
+        }
     }
 
     private class Node {
