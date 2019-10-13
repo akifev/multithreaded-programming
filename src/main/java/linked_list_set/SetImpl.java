31515c7a4480be1a17746775a18d854c2cd1ccff
Akifev Daniil <akifevdaniil@yandex.ru>
2019-10-13 23:35:02 +0300

    done

--- src/main/java/linked_list_set/SetImpl.java
+++ src/main/java/linked_list_set/SetImpl.java
@@ -1,66 +1,81 @@
 package linked_list_set;
 
+import kotlinx.atomicfu.AtomicRef;
+
 public class SetImpl implements Set {
+
+    private class Removed extends Node {
+        Removed(int x, Node next) {
+            super(x, next);
+        }
+    }
+
     private class Node {
-        Node next;
         int x;
+        AtomicRef<Node> next;
 
         Node(int x, Node next) {
-            this.next = next;
             this.x = x;
+            this.next = new AtomicRef<>(next);
         }
     }
 
     private class Window {
-        Node cur, next;
+        Node cur;
+        Node next;
     }
 
-    private final Node head = new Node(Integer.MIN_VALUE, new Node(Integer.MAX_VALUE, null));
+    private final AtomicRef<Node> head = new AtomicRef<Node>(new Node(Integer.MIN_VALUE, new Node(Integer.MAX_VALUE, null)));
 
-    /**
-     * Returns the {@link Window}, where cur.x < x <= next.x
-     */
+    private boolean isRemoved(Node node) {
+        return node.next.getValue() instanceof Removed;
+    }
+
     private Window findWindow(int x) {
-        Window w = new Window();
-        w.cur = head;
-        w.next = w.cur.next;
-        while (w.next.x < x) {
-            w.cur = w.next;
-            w.next = w.cur.next;
+        while (true) {
+            Window w = new Window();
+            w.cur = head.getValue();
+            w.next = w.cur.next.getValue();
+            while (w.next.x < x || isRemoved(w.next)) {
+                if (isRemoved(w.cur)) break;
+                if (isRemoved(w.next)) {
+                    Node node = w.next.next.getValue().next.getValue();
+                    if (!w.cur.next.compareAndSet(w.next, node)) {
+                        w.next = w.cur.next.getValue();
+                        continue;
+                    }
+                    w.next = node;
+                } else {
+                    w.cur = w.next;
+                    w.next = w.cur.next.getValue();
+                }
+            }
+            if (isRemoved(w.cur)) continue;
+            return w;
         }
-        return w;
     }
 
     @Override
     public boolean add(int x) {
-        Window w = findWindow(x);
-        boolean res;
-        if (w.next.x == x) {
-            res = false;
-        } else {
-            w.cur.next = new Node(x, w.next);
-            res = true;
+        while (true) {
+            Window w = findWindow(x);
+            if (w.next.x == x) return false;
+            if (w.cur.next.compareAndSet(w.next, new Node(x, w.next))) return true;
         }
-        return res;
     }
 
     @Override
     public boolean remove(int x) {
-        Window w = findWindow(x);
-        boolean res;
-        if (w.next.x != x) {
-            res = false;
-        } else {
-            w.cur.next = w.next.next;
-            res = true;
+        while (true) {
+            Window w = findWindow(x + 1);
+            if (w.cur.x != x) return false;
+            if (w.cur.next.compareAndSet(w.next, new Removed(Integer.MIN_VALUE, w.next))) return true;
         }
-        return res;
     }
 
     @Override
     public boolean contains(int x) {
         Window w = findWindow(x);
-        boolean res = w.next.x == x;
-        return res;
+        return w.next.x == x;
     }
 }
