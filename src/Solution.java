a5ba9444c7c312a1cb94c1ebb44f49e7decc2483
Daniil Akifev <akifev.ru@gmail.com>
2019-11-16 05:20:50 +0300

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,27 @@
+public class Solution implements AtomicCounter {
+
+    private final Node head = new Node(0);
+    private final ThreadLocal<Node> tail =
+            ThreadLocal.withInitial(() -> head);
+
+    public int getAndAdd(int x) {
+        while (true) {
+            final int oldValue = tail.get().value;
+            final int newValue = oldValue + x;
+            final Node newNode = new Node(newValue);
+            tail.set(tail.get().next.decide(newNode));
+            if (tail.get() == newNode) {
+                return oldValue;
+            }
+        }
+    }
+
+    private static class Node {
+        final int value;
+        final Consensus<Node> next = new Consensus<>();
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+}
