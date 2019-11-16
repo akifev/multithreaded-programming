4cdc8ae2fb4ba1b0979e81096fbf438137dda69a
Daniil Akifev <akifev.ru@gmail.com>
2019-11-16 19:03:39 +0300

    COMMIT

--- src/main/java/faaqueue/FAAQueue.java
+++ src/main/java/faaqueue/FAAQueue.java
@@ -1,66 +1,84 @@
 package faaqueue;
 
-import kotlinx.atomicfu.*;
+import kotlinx.atomicfu.AtomicArray;
+import kotlinx.atomicfu.AtomicInt;
+import kotlinx.atomicfu.AtomicRef;
 
-import static faaqueue.FAAQueue.Node.NODE_SIZE;
-
+import static faaqueue.FAAQueue.Segment.SEGMENT_SIZE;
 
 public class FAAQueue<T> implements Queue<T> {
-    private static final Object DONE = new Object(); // Marker for the "DONE" slot state; to avoid memory leaks
+    private static final Object DONE = new Object();
 
-    private AtomicRef<Node> head; // Head pointer, similarly to the Michael-Scott queue (but the first node is _not_ sentinel)
-    private AtomicRef<Node> tail; // Tail pointer, similarly to the Michael-Scott queue
+    private AtomicRef<Segment> head;
+    private AtomicRef<Segment> tail;
 
     public FAAQueue() {
-        Node firstNode = new Node();
-        head = new AtomicRef<>(firstNode);
-        tail = new AtomicRef<>(firstNode);
+        Segment firstSegment = new Segment();
+        head = new AtomicRef<>(firstSegment);
+        tail = new AtomicRef<>(firstSegment);
     }
 
     @Override
     public void enqueue(T x) {
-        int enqIdx = this.tail.getValue().enqIdx++;
-        if (enqIdx >= NODE_SIZE) {
-            Node newTail = new Node(x);
-            this.tail.getValue().next = newTail;
-            this.tail.setValue(newTail);
-            return;
+        while (true) {
+            Segment last = this.tail.getValue();
+            int enqIdx = last.enqIdx.getAndIncrement();
+            if (enqIdx >= SEGMENT_SIZE) {
+                Segment newTail = new Segment(x);
+                if (last.next.compareAndSet(null, newTail)) {
+                    this.tail.compareAndSet(last, newTail);
+                    return;
+                }
+                this.tail.compareAndSet(last, last.next.getValue());
+            } else {
+                if (last.data.get(enqIdx).compareAndSet(null, x)) {
+                    return;
+                }
+            }
         }
-        this.tail.getValue().data[enqIdx] = x;
     }
 
     @Override
     public T dequeue() {
         while (true) {
-            if (this.head.getValue().isEmpty()) {
-                if (this.head.getValue().next == null) return null;
-                this.head.setValue(this.head.getValue().next);
+            Segment first = this.head.getValue();
+            Segment firstNext = first.next.getValue();
+            if (first.isEmpty()) {
+                if (firstNext == null) {
+                    return null;
+                }
+                this.head.compareAndSet(first, firstNext);
                 continue;
             }
-            int deqIdx = this.head.getValue().deqIdx++;
-            Object res = head.getValue().data[deqIdx];
-            head.getValue().data[deqIdx] = DONE;
+            int deqIdx = first.deqIdx.getAndIncrement();
+            if (deqIdx >= SEGMENT_SIZE) {
+                continue;
+            }
+            Object res = first.data.get(deqIdx).getAndSet(DONE);
+            if (res == null) {
+                continue;
+            }
             return (T) res;
         }
     }
 
-    static class Node {
-        static final int NODE_SIZE = 2; // CHANGE ME FOR BENCHMARKING ONLY
+    static class Segment {
+        static final int SEGMENT_SIZE = 2;
+        private final AtomicArray<Object> data = new AtomicArray<>(SEGMENT_SIZE);
+        private AtomicRef<Segment> next = new AtomicRef<>(null);
+        private AtomicInt enqIdx = new AtomicInt(0);
+        private AtomicInt deqIdx = new AtomicInt(0);
 
-        private Node next = null;
-        private int enqIdx = 0; // index for the next enqueue operation
-        private int deqIdx = 0; // index for the next dequeue operation
-        private final Object[] data = new Object[NODE_SIZE];
+        Segment() {
+        }
 
-        Node() {}
-
-        Node(Object x) {
-            this.enqIdx = 1;
-            this.data[0] = x;
+        Segment(Object x) {
+            this.enqIdx.setValue(1);
+            this.data.get(0).setValue(x);
         }
 
         private boolean isEmpty() {
-            return this.deqIdx >= this.enqIdx || this.deqIdx >= NODE_SIZE;
+            return this.deqIdx.getValue() >= this.enqIdx.getValue() || this.deqIdx.getValue() >= SEGMENT_SIZE;
         }
     }
 }
