955b78773cbbdacf0c1593061bdc1836e16fafed
Daniil Akifev <akifev.ru@gmail.com>
2019-11-17 20:05:09 +0300

    sol

--- src/main/java/dijkstra/Dijkstra.kt
+++ src/main/java/dijkstra/Dijkstra.kt
@@ -1,5 +1,6 @@
 package dijkstra
 
+import kotlinx.atomicfu.atomic
 import java.util.*
 import java.util.concurrent.Phaser
 import kotlin.Comparator
@@ -10,32 +11,82 @@ private val NODE_DISTANCE_COMPARATOR = Comparator<Node> { o1, o2 -> Integer.comp
 // Returns `Integer.MAX_VALUE` if a path has not been found.
 fun shortestPathParallel(start: Node) {
     val workers = Runtime.getRuntime().availableProcessors()
-    // The distance to the start node is `0`
     start.distance = 0
-    // Create a priority (by distance) queue and add the start node into it
-    val q = PriorityQueue(workers, NODE_DISTANCE_COMPARATOR) // TODO replace me with a multi-queue based PQ!
+    val q = MultiPriorityQueue(workers, NODE_DISTANCE_COMPARATOR)
     q.add(start)
-    // Run worker threads and wait until the total work is done
-    val onFinish = Phaser(workers + 1) // `arrive()` should be invoked at the end by each worker
+    val onFinish = Phaser(workers + 1)
+    val activeNodes = atomic(1)
     repeat(workers) {
         thread {
             while (true) {
-                // TODO Write the required algorithm here,
-                // TODO break from this loop when there is no more node to process.
-                // TODO Be careful, "empty queue" != "all nodes are processed".
-//                val cur: Node? = synchronized(q) { q.poll() }
-//                if (cur == null) {
-//                    if (workIsDone) break else continue
-//                }
-//                for (e in cur.outgoingEdges) {
-//                    if (e.to.distance > cur.distance + e.weight) {
-//                        e.to.distance = cur.distance + e.weight
-//                        q.addOrDecreaseKey(e.to)
-//                    }
-//                }
+                val cur: Node = q.poll() ?: if (activeNodes.value > 0) continue else break
+                for (e in cur.outgoingEdges) {
+                    CASloop@ while (true) {
+                        val curDistance = e.to.distance
+                        val newDistance = cur.distance + e.weight
+                        if (newDistance >= curDistance) {
+                            break@CASloop
+                        }
+                        if (e.to.casDistance(curDistance, newDistance)) {
+                            q.add(e.to)
+                            activeNodes.getAndIncrement()
+                            break@CASloop
+                        }
+                    }
+                }
+                activeNodes.getAndDecrement()
             }
             onFinish.arrive()
         }
     }
     onFinish.arriveAndAwaitAdvance()
+}
+
+class MultiPriorityQueue(workers: Int, nodeDistanceComparator: Comparator<Node>) {
+    private val list = arrayListOf<PriorityQueue<Node>>()
+    private val size: Int = workers * 2;
+
+    init {
+        for (i in 0 until size) {
+            list.add(PriorityQueue(nodeDistanceComparator));
+        }
+    }
+
+    fun add(node: Node) {
+        val index: Int = (0 until size).random()
+        synchronized(list[index]) {
+            list[index].add(node)
+        }
+    }
+
+    fun poll(): Node? {
+        for (i in 0..3) {
+            val first: Int = (0 until size).random()
+            val second: Int = (0 until size).random()
+            if (first == second) {
+                continue;
+            }
+            synchronized(list[first]) {
+                synchronized(list[second]) {
+                    val firstMin: Node? = list[first].peek()
+                    val secondMin: Node? = list[second].peek()
+                    if (firstMin != null && secondMin != null) {
+                        if (firstMin.distance < secondMin.distance) {
+                            return list[first].poll()
+                        } else {
+                            return list[second].poll()
+                        }
+                    }
+                }
+            }
+        }
+        for (index in 0 until size) {
+            synchronized(list[index]) {
+                if (list[index].size > 0) {
+                    return list[index].poll()
+                }
+            }
+        }
+        return null
+    }
 }
