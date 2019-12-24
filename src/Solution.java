c3be343200171d8f42621393d7c7c5ad1b290488
akifev <akifev.ru@gmail.com>
2019-12-25 00:25:28 +0300

    revert

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,45 @@
-import java.util.concurrent.atomic.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Solution implements Lock<Solution.Node> {
     private final Environment env;
+    private final AtomicReference<Node> last;
+
     public Solution(Environment env) {
         this.env = env;
+        last = new AtomicReference<>(null);
     }
 
     @Override
     public Node lock() {
-        Node my = new Node(); // сделали узел
-        while (my.locked.get()) env.park();
-        return my; // вернули узел
+        Node cur = new Node();
+        Node prev = last.getAndSet(cur);
+        if (prev != null) {
+            prev.next.set(cur);
+            while (cur.locked.get()) {
+                env.park();
+            }
+        }
+
+        return cur;
     }
 
     @Override
     public void unlock(Node node) {
-        node.locked.set(false);
-        env.unpark(node.thread);
+        if (node.next.get() == null) {
+            if (last.compareAndSet(node, null)) {
+                return;
+            }
+            while (node.next.get() == null) {
+                // Nothing to do here
+            }
+        }
+        node.next.get().locked.set(false);
+        env.unpark(node.next.get().thread);
     }
 
     static class Node {
-        final Thread thread = Thread.currentThread(); // запоминаем поток, которые создал узел
+        final Thread thread = Thread.currentThread();
         final AtomicReference<Boolean> locked = new AtomicReference<>(true);
+        final AtomicReference<Node> next = new AtomicReference<>(null);
     }
 }
